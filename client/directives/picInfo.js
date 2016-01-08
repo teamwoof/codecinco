783b2a7193f76c362ed9a6aa0f509e225bb9d4cb
Kayla Matteo <kaylamatteo@gmail.com>
2016-01-08 10:53:43 -0600

    rating bug fix

--- client/directives/picInfo.js
+++ client/directives/picInfo.js
@@ -10,16 +10,10 @@ angular.module('myApp')
         // scope.confirm = info.confirm;
         // info.confirm = scope.confirm;
               scope.goToSingleOutfit = function(imageUrl, imageId){
-                console.log('imageID', imageId);
-                console.log('imageUrl', imageUrl);
                 $state.go('singleOutfit', {imageUrl: imageUrl, imageId: imageId});
               };
               scope.confirmDelete = function(choice){
-                if (choice === 1){
-                  scope.info.confirm = true;
-                } else {
-                  scope.info.confirm = false;
-                }
+                scope.info.confirm = choice ? true : false;
              };
              scope.deleteImage = function(imageId, imageName){
               scope.info.remove(scope.info.image_id, scope.info.image_name);
